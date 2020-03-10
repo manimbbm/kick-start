ec67fb7b934edc329e3dc01aa65399328b2a643b
brunobme <marqueisbruno@gmail.com>
2020-03-10 07:17:04 +0000

    round-h-2019

---
+++ h-index.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+	int t = NULL, n, i;
+	t = 2;
+	if(t){
+		printf("true\n");
+	}else{
+		printf("false\n");
+	}
+	// scanf("%d", &t);
+	// scanf("%d", &n);
+	// int A[n];
+	// for(i = 0; i < n; i++){
+	// 	scanf("%d", A[i]);
+	// }
+	
+	return 0;
+}
---
+++ index.js
@@ -0,0 +1,3 @@
+
+var foo = readline();
+console.log(foo)
