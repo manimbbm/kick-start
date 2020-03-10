01f704d4eefca82230b7d408543bf177ac62abee
Bruno Marques <bmarques@poppulo.com>
2020-03-10 07:33:02 +0000

    Decrementally order them?

--- h-index.c
+++ h-index.c
@@ -2,19 +2,15 @@
 #include <stdio.h>
 
 int main(){
-	int t = NULL, n, i;
-	t = 2;
-	if(t){
-		printf("true\n");
-	}else{
-		printf("false\n");
+	int t, n, i;
+
+	scanf("%d", &t);
+	scanf("%d", &n);
+	int A[n];
+	for(i = 0; i < n; i++){
+		scanf("%d", A[i]);
 	}
-	// scanf("%d", &t);
-	// scanf("%d", &n);
-	// int A[n];
-	// for(i = 0; i < n; i++){
-	// 	scanf("%d", A[i]);
-	// }
+	//maybe try to decrementally order them so that I always know the number of citations on the ith paper has h-index max(ith, curr citations, old max-h-index)
 	
 	return 0;
 }
