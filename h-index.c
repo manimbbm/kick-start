7d7560700b627c3bc0b9b8695dc0ced86dd12ca3
Bruno Marques <bmarques@poppulo.com>
2020-03-13 07:41:11 +0000

    Fixed swap, created main h_index method, which is not working yet

--- h-index.c
+++ h-index.c
@@ -1,26 +1,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void swap (int x, int y){
-    printf("x %p y %p\n", x, y);
-    int * temp = &x;
-    &x = &y;
-    &y = temp;
-    printf("x %p y %p", x, y);
-    // not working
+void swap (int *x, int *y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
 
 void reverse (int arr[], int start, int end) {
-    // works fine if uncommenting and removing broken swap
     int temp, i;
     for (start; start < end; start++, end--) {
-        // temp = arr[start];
-        // arr[start] = arr[end];
-        // arr[end] = temp;
-        swap(arr[start], arr[end]);
+        swap(arr + start, arr + end);
     }
+}
 
-    return;
+int max (int x, int y) {
+	if (x >= y) {
+		return x;
+	}
+	return y;
+}
+
+int min (int x, int y) {
+	if (x >= y) {
+		return y;
+	}
+	return x;
+}
+
+void h_index (int papers[], int n) {
+	int cur_max = 1;
+    for (int i = 1; i < n + 1; i++) {
+        cur_max = max(min(i, papers[i]), cur_max);
+    }
+    printf("%d\n", cur_max);
 }
 
 int main() {
@@ -35,13 +48,12 @@ int main() {
 	    for(j = 0; j < n; j++){
             scanf("%d", &A[j]);
         }
-
-	    reverse(A, 0, n-1);
-	    for(j = 0; j < n; j++){
-            printf(" %d", A[j]);
-        }
-        printf("\n");
+        
+        reverse(A, 0, n-1);
+		h_index(A, t);
 	}
+	
 	//maybe try to decrementally order them so that I always know the number of citations on the ith paper has h-index max(ith, curr citations, old max-h-index)
+	
 	return 0;
 }
