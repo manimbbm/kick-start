1594f7b8d61d294ecd2c3338722b90f4cf758912
Bruno Marques <bmarques@poppulo.com>
2020-03-16 08:02:48 +0000

    fixed h-index, missing sort before reverse

--- h-index.c
+++ h-index.c
@@ -1,39 +1,52 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// verified
 void swap (int *x, int *y){
     int temp = *x;
     *x = *y;
     *y = temp;
 }
 
+// verified
 void reverse (int arr[], int start, int end) {
-    int temp, i;
+    int temp;
+    // debug: 
+    int i, n = end;
     for (start; start < end; start++, end--) {
-        swap(arr + start, arr + end);
+        swap(arr + start, arr + end - 1);
     }
+    // debug:
+    printf("arr: ");
+    for (i = 0; i < n; i ++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");	
 }
 
-int max (int x, int y) {
+int max_c (int x, int y) {
 	if (x >= y) {
 		return x;
 	}
 	return y;
 }
 
-int min (int x, int y) {
+int min_c (int x, int y) {
 	if (x >= y) {
 		return y;
 	}
 	return x;
 }
 
-void h_index (int papers[], int n) {
-	int cur_max = 1;
+void h_index (int papers[], int n, int cur_case) {
+	int cur_max = 1, cur_min = 1;
+    printf("Case #%d: ", cur_case);
     for (int i = 1; i < n + 1; i++) {
-        cur_max = max(min(i, papers[i]), cur_max);
+    	cur_min = min_c(i, papers[i-1]);
+        cur_max = max_c(cur_min, cur_max);
+        printf("%d ", cur_max);
     }
-    printf("%d\n", cur_max);
+    printf("\n");
 }
 
 int main() {
@@ -41,7 +54,6 @@ int main() {
 
 	scanf("%d", &t);
 	for (i = 1; i < t + 1; i++) {
-	    printf("Case #%d:", i);
 	    scanf("%d", &n);
 	    int A[n];
 
@@ -49,11 +61,9 @@ int main() {
             scanf("%d", &A[j]);
         }
         
-        reverse(A, 0, n-1);
-		h_index(A, t);
+        reverse(A, 0, n);
+		h_index(A, n, i);
 	}
-	
-	//maybe try to decrementally order them so that I always know the number of citations on the ith paper has h-index max(ith, curr citations, old max-h-index)
 	
 	return 0;
 }
