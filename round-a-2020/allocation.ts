52448c6b986fdef242e450620488445bae21ddf1
manimbbm <marqueisbruno@gmail.com>
2020-05-05 08:59:50 +0100

    adding standard input

--- round-a-2020/allocation.ts
+++ round-a-2020/allocation.ts
@@ -1,7 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
 let budget = 300;
+
+const inputBudget = () => {
+	rl.question("What is your budget?", (answer) => {
+		budget = answer;
+		rl.close();
+	});
+};
+
+inputBudget();
+
 let houses = [999, 999, 999, 990];
 
-function allocation(budget, houses) {
+const allocate = (budget, houses) => {
 	houses.sort();
 	let counter = 0;
 	let max_houses = 0;
@@ -17,4 +34,4 @@ function allocation(budget, houses) {
 	return max_houses;
 }
 
-console.log(allocation(budget, houses));
+console.log(allocate(budget, houses));
