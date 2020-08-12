ab3d971aacc803050028dba8421821342f1dbcfb
manimbbm <marqueisbruno@gmail.com>
2020-08-12 08:12:04 +0100

    Optimized a little by checking if sum is include in a perfect square array instead of taking the sqrt from all. Getting Time Limit Exceeded

--- round-c-2020/perfect-array.js
+++ round-c-2020/perfect-array.js
@@ -6,21 +6,44 @@ function readline() {
     return input[line++];
 }
 var t = readline();
+var squares_1000 = allSquaresBellowN(1000);
+var squares_105 = allSquaresBellowN(Math.pow(10, 5));
 for (var i = 1; i <= t; i++) {
     var N = readline();
     var arr = readline().split(" ").map(function (x) { return +x; });
-    console.log("Case #" + i + ": " + perfectArray(arr));
+    console.log("Case #" + i + ": " + perfectArray(arr, N));
 }
-function perfectArray(arr) {
+function perfectArray(arr, N) {
+    var perfectSquares;
+    if (N > 1000) {
+        perfectSquares = squares_105;
+    }
+    else {
+        perfectSquares = squares_1000;
+    }
     var ans = 0;
+    // console.log("N", N);
+    // console.log("perfectSquares", perfectSquares);
     //contiguous sub arrays
     for (var i = 0; i < arr.length; i++) {
         for (var j = i + 1; j <= arr.length; j++) {
-            if (Math.sqrt(arr.slice(i, j).reduce(function (a, b) { return a + b; }, 0)) % 1 == 0) {
+            //optimizing by checking a list with all perfect squares possible
+            if (perfectSquares.includes(arr.slice(i, j).reduce(function (a, b) { return a + b; }, 0))) {
+                // console.log('arr.slice(i, j).reduce((a, b) => a + b, 0)', arr.slice(i, j).reduce((a, b) => a + b, 0));
                 ans++;
             }
         }
     }
-    console.log('ans', ans);
     return ans;
+}
+function allSquaresBellowN(N) {
+    var max = 100 * N, i = 1;
+    var squares = [0];
+    var result = 1;
+    while (result < max) {
+        squares.push(result);
+        i++;
+        result = i * i;
+    }
+    return squares;
 }
--- round-c-2020/perfect-array.ts
+++ round-c-2020/perfect-array.ts
@@ -10,24 +10,48 @@ function readline(){
 
 let t = readline();
 
+let squares_1000 = allSquaresBellowN(1000);
+let squares_105 = allSquaresBellowN(Math.pow(10, 5));
+
 for (let i = 1; i <= t; i++) {
 	let N = readline();
 	let arr = readline().split(" ").map(x => +x);
-	console.log(`Case #${i}: ${perfectArray(arr)}`)
+	console.log(`Case #${i}: ${perfectArray(arr, N)}`);
 }
 
-function perfectArray (arr: []) {
+
+function perfectArray (arr: [], N: number) {
+	let perfectSquares: number[];
+	if (N > 1000) {
+		perfectSquares = squares_105;
+	} else {
+		perfectSquares = squares_1000;
+	}
 	let ans: number = 0;
+	// console.log("N", N);
+	// console.log("perfectSquares", perfectSquares);
 
 	//contiguous sub arrays
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = i + 1; j <= arr.length; j++) {
-			if (Math.sqrt(arr.slice(i, j).reduce((a, b) => a + b, 0)) % 1 == 0) {
+			//optimizing by checking a list with all perfect squares possible
+			if (perfectSquares.includes(arr.slice(i, j).reduce((a, b) => a + b, 0))) {
+				// console.log('arr.slice(i, j).reduce((a, b) => a + b, 0)', arr.slice(i, j).reduce((a, b) => a + b, 0));
 				ans++;
 			}
 		}
 	}
-
-	console.log('ans', ans);
 	return ans;
+}
+
+function allSquaresBellowN(N: number): number[] {
+	let max = 100 * N, i = 1;
+	let squares: number[] = [0];
+	let result = 1;
+	while (result < max) {
+		squares.push(result);
+		i++;
+		result = i * i;
+	}
+	return squares;
 }
