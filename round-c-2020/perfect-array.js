3f0c0f1bfbe592a2b7f7969970051ebf7080a8c7
manimbbm <marqueisbruno@gmail.com>
2020-08-13 08:12:56 +0100

    Sqrt version is two times faster. Replaced slice and reduce to adding the values as I go.

--- round-c-2020/perfect-array.js
+++ round-c-2020/perfect-array.js
@@ -11,7 +11,15 @@ var squares_105 = allSquaresBellowN(Math.pow(10, 5));
 for (var i = 1; i <= t; i++) {
     var N = readline();
     var arr = readline().split(" ").map(function (x) { return +x; });
-    console.log("Case #" + i + ": " + perfectArray(arr, N));
+    // console.time(`perfectArray iteration ${i}`);
+    // console.log(`Case #${i}: ${perfectArray(arr, N)}`);
+    // console.timeEnd(`perfectArray iteration ${i}`);
+    // runs in half the time...
+    // why?
+    // Math.sqrt not as expensive as a .includes
+    console.time("perfectArraySqrtVersion( iteration " + i);
+    console.log("Case #" + i + ": " + perfectArraySqrtVersion(arr));
+    console.timeEnd("perfectArraySqrtVersion( iteration " + i);
 }
 function perfectArray(arr, N) {
     var perfectSquares;
@@ -27,11 +35,33 @@ function perfectArray(arr, N) {
     //contiguous sub arrays
     for (var i = 0; i < arr.length; i++) {
         for (var j = i + 1; j <= arr.length; j++) {
-            //optimizing by checking a list with all perfect squares possible
+            //optimizing by checking a list with all perfect squares possible? Edit: turned out to double the time. Why?
             if (perfectSquares.includes(arr.slice(i, j).reduce(function (a, b) { return a + b; }, 0))) {
                 // console.log('arr.slice(i, j).reduce((a, b) => a + b, 0)', arr.slice(i, j).reduce((a, b) => a + b, 0));
                 ans++;
             }
+        }
+    }
+    return ans;
+}
+function perfectArraySqrtVersion(arr) {
+    var ans = 0;
+    var sum = 0;
+    //contiguous sub arrays
+    for (var i = 0; i < arr.length; i++) {
+        sum = arr[i];
+        if (Math.sqrt(sum) % 1 == 0) {
+            ans++;
+        }
+        for (var j = i + 1; j < arr.length; j++) {
+            sum += arr[j];
+            if (Math.sqrt(sum) % 1 == 0) {
+                ans++;
+            }
+            // if (Math.sqrt(arr.slice(i, j).reduce((a, b) => a + b, 0)) % 1 == 0) {
+            // 	// console.log('arr.slice(i, j).reduce((a, b) => a + b, 0)', arr.slice(i, j).reduce((a, b) => a + b, 0));
+            // 	ans++;
+            // }
         }
     }
     return ans;
@@ -47,13 +77,3 @@ function allSquaresBellowN(N) {
     }
     return squares;
 }
-function printNumbers(N) {
-    var i = 1;
-    var numbers = [+(Math.random() * 100).toFixed()];
-    while (i < N) {
-        numbers.push(+(Math.random() * 100).toFixed());
-        i++;
-    }
-    console.dir(numbers, { 'maxArrayLength': N });
-}
-printNumbers(1000);
--- round-c-2020/perfect-array.ts
+++ round-c-2020/perfect-array.ts
@@ -16,9 +16,17 @@ let squares_105 = allSquaresBellowN(Math.pow(10, 5));
 for (let i = 1; i <= t; i++) {
 	let N = readline();
 	let arr = readline().split(" ").map(x => +x);
-	console.log(`Case #${i}: ${perfectArray(arr, N)}`);
-}
+	// console.time(`perfectArray iteration ${i}`);
+	// console.log(`Case #${i}: ${perfectArray(arr, N)}`);
+	// console.timeEnd(`perfectArray iteration ${i}`);
 
+	// runs in half the time...
+	// why?
+	// Math.sqrt not as expensive as a .includes
+	console.time(`perfectArraySqrtVersion( iteration ${i}`);
+	console.log(`Case #${i}: ${perfectArraySqrtVersion(arr)}`);
+	console.timeEnd(`perfectArraySqrtVersion( iteration ${i}`);
+}
 
 function perfectArray (arr: [], N: number) {
 	let perfectSquares: number[];
@@ -34,11 +42,34 @@ function perfectArray (arr: [], N: number) {
 	//contiguous sub arrays
 	for (let i = 0; i < arr.length; i++) {
 		for (let j = i + 1; j <= arr.length; j++) {
-			//optimizing by checking a list with all perfect squares possible
+			//optimizing by checking a list with all perfect squares possible? Edit: turned out to double the time. Why?
 			if (perfectSquares.includes(arr.slice(i, j).reduce((a, b) => a + b, 0))) {
 				// console.log('arr.slice(i, j).reduce((a, b) => a + b, 0)', arr.slice(i, j).reduce((a, b) => a + b, 0));
 				ans++;
 			}
+		}
+	}
+	return ans;
+}
+
+function perfectArraySqrtVersion (arr: []) {
+	let ans: number = 0;
+	let sum: number = 0;
+	//contiguous sub arrays
+	for (let i = 0; i < arr.length; i++) {
+		sum = arr[i];
+		if (Math.sqrt(sum) % 1 == 0) {
+			ans++;
+		}
+		for (let j = i + 1; j < arr.length; j++) {
+			sum += arr[j];
+			if (Math.sqrt(sum) % 1 == 0) {
+				ans++;
+			}
+			// if (Math.sqrt(arr.slice(i, j).reduce((a, b) => a + b, 0)) % 1 == 0) {
+			// 	// console.log('arr.slice(i, j).reduce((a, b) => a + b, 0)', arr.slice(i, j).reduce((a, b) => a + b, 0));
+			// 	ans++;
+			// }
 		}
 	}
 	return ans;
