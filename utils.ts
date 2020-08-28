787e3a860778e8d66ef22acf30e98b8cfebc381e
manimbbm <marqueisbruno@gmail.com>
2020-08-28 08:05:44 +0100

    Improving random numbers util

--- utils.ts
+++ utils.ts
@@ -1,14 +1,20 @@
 const fs = require('fs');
 
-function printNumbers(N: number) {
+function printRandomNumbers(N: number, max:number) {
     let i = 1;
-    let numbers: number[] = [+(Math.random()*100).toFixed()];
-    while (i < N) {
-        numbers.push(+(Math.random()*100).toFixed());
-        i++;
+    if (N && max) {
+        let numbers: number[] = [+(Math.random()*Math.pow(10, max.toString().length)).toFixed()];
+        while (i < N) {
+            numbers.push(+(Math.random()*Math.pow(10, max.toString().length)).toFixed());
+            i++;
+        }
+        fs.writeFileSync('utils.txt', numbers.join(' '));
+        console.dir(numbers, {'maxArrayLength': N});
+    } else {
+        console.log('Please pass on as parameters 1- the total random N you\'d like to generate and 2- the max value "node utils {N} {max}"\n' +
+            'Example: "node utils 5 900"');
     }
-    fs.writeFileSync('utils.txt', numbers.join(' '));
-    // console.dir(numbers, {'maxArrayLength': N});
+
 }
 
-printNumbers(1e5);
+printRandomNumbers(+process.argv[2], +process.argv[3]);
