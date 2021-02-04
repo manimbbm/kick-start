705b2a6a1edcbcd63e8fd32d0b67d51c52369128
manimbbm <marqueisbruno@gmail.com>
2021-02-04 08:29:34 +0000

    Improved ifs but it is still missing a few boring numbers

--- 2020/round-h/boring-numbers.ts
+++ 2020/round-h/boring-numbers.ts
@@ -16,6 +16,7 @@ let t = readline();
 for (let i = 1; i <= t; i++) {
 	let [L, R] = readline().split(' ').map(x => +x);
 	console.log(`Case #${i}: ${smart_check_with_splits_and_permutations(L, R)}`)
+	console.log(`Case greedy #${i}: ${greedy_check_each_number(L, R)}`)
 }
 
 
@@ -44,7 +45,9 @@ function smart_check_with_splits_and_permutations(L: number, R: number) {
 	//find out each side closest "dezimal" number
 	let closestDecL = closestDec(L);
 	let closestDecR = closestDec(R);
-	boring_permutations_up_to_n(closestDecL);
+	let permutationsToL = boring_permutations_up_to_n(L, false);
+	let permutationsToR = boring_permutations_up_to_n(R, true);
+
 	console.log({
 		L,
 		closestDecL,
@@ -57,7 +60,7 @@ function smart_check_with_splits_and_permutations(L: number, R: number) {
 
     // subtract the ones greater than R (testEach one with new optimized approach)
 	// subtract the ones lower than L (testEach one with new optimized approach)
-	return count;
+	return permutationsToR - permutationsToL;
 }
 
 function closestDec(n: number): number {
@@ -76,25 +79,76 @@ function is_boring_number(n: number): boolean {
 
 }
 
-function boring_permutations_up_to_n(n: number) {
+function boring_permutations_up_to_n(n: number, inclusive: boolean) {
+	// issue, cant count any "dezen" number if the previous is not boring example 650, cant count up to 50 cause the initial 6 makes the following not boring already 
 	console.log("boring permutations");
 
 	let stringN = String(n);
-	let firstDigit = +stringN[0];
+	
+	let result = 0, 
+	firstDigitPossibilities = 0, 
+	i = 1, 
+	j, 
+	firstDigit, 
+	lastDigitIndex = stringN.length - 1;
 
-	let i = stringN.length % 2 != 0 ? 1 : 0;
-	let firstDigitPossibilities = 0;
-	for(i; i < firstDigit; i += 2) {
-		firstDigitPossibilities++; 
-	}
+	for (let index = 0; index < stringN.length; index ++ ) {
+		let currStr = stringN.substring(index, stringN.length);
+		firstDigit = +stringN[index];
+		firstDigitPossibilities = 0;
+		j = i;
 
-	let result = 0;
+		for(j; j < firstDigit; j += 2) {
+			firstDigitPossibilities++; 
+		}
+	
+		if (inclusive && index === lastDigitIndex) {
+			for(j; j <= firstDigit; j += 2) {
+				firstDigitPossibilities++; 
+			}
+		} 	
+		
+				
+		
+		if (index != lastDigitIndex && firstDigitPossibilities != 0) {
+			console.log("result Math.pow(5, (currStr.length - 1)) * firstDigitPossibilities")
+			result += Math.pow(5, (currStr.length - 1)) * firstDigitPossibilities			
+		} else if (index === lastDigitIndex) {
+			console.log("result firstDigitPossibilities")
+			result += firstDigitPossibilities;
+		} else if (firstDigitPossibilities == 0) {
+			console.log("result Math.pow(5, (currStr.length - 1))")
+			result += Math.pow(5, (currStr.length - 1));
+		}
 
-	//continue
-	stringN.split("").forEach(c => {
+		i = Math.abs(i - 1);
+		
+		let currStrL = currStr.length;
+		
+		//todo check those missing numbers up to EACH "dezen"
+		if (index != lastDigitIndex) {
+			for (currStrL; currStrL > 0; currStrL--) {
+				result += Math.pow(5, (currStr.length - 1));
+			}
+		} 
+
+		if (j != firstDigit) {
+			// then the first digit is not included and should return result already
+			break;
+		}
+		
 		console.log({
-			stringN,
-			c
-		})
-	});
+			currStr,
+			i,
+			j,
+			result
+		});
+	};
+
+	
+	console.log({
+		stringN,
+		result
+	})
+	return result;
 }
